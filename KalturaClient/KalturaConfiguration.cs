72efc3c8f2017f0aafef908717d1aa54504233fc
Kaltura CSI <community@kaltura.com>
2015-06-01 00:27:45 -0400

    New clientlib ver

--- KalturaClient/KalturaConfiguration.cs
+++ KalturaClient/KalturaConfiguration.cs
@@ -40,7 +40,7 @@ namespace Kaltura
         private int _PartnerId;
         private IKalturaLogger _Logger;
         private int _Timeout = 120000;
-        private string _ClientTag = "dotnet:15-05-31";
+        private string _ClientTag = "dotnet:15-06-01";
 		private string _ProxyAddress = "";
 		private string _ProxyUser = null;
         private string _ProxyPassword = null;

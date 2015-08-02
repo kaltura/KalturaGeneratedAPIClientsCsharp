67c4c1a5eed0e3e80969b685a9b440808931f1f7
Jess Portnoy <jess.portnoy@kaltura.com>
2015-08-02 18:02:08 +0100

    somehow got lost..

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash - 
+#===============================================================================
+#          FILE: build.sh
+#         USAGE: ./build.sh 
+#   DESCRIPTION: 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Jess Portnoy (), <jess.portnoy@kaltura.com>
+#  ORGANIZATION: Kaltura, inc.
+#       CREATED: 05/20/2015 06:03:52 PM IDT
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+xbuild KalturaClient.sln
+cd ./KalturaClientTester/bin/Debug/ &&  mono KalturaClientTester.exe
+

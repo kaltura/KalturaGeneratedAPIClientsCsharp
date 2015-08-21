1fc497fbed09237ec5c93715b8a9eb72c211a174
Kaltura CSI <community@kaltura.com>
2015-08-21 06:39:37 -0400

    New clientlib ver

---
+++ KalturaClient/Types/KalturaIntegrationJobData.cs
@@ -0,0 +1,141 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2015  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+using System;
+using System.Xml;
+using System.Collections.Generic;
+
+namespace Kaltura
+{
+	public class KalturaIntegrationJobData : KalturaJobData
+	{
+		#region Private Fields
+		private string _CallbackNotificationUrl = null;
+		private KalturaIntegrationProviderType _ProviderType = null;
+		private KalturaIntegrationJobProviderData _ProviderData;
+		private KalturaIntegrationTriggerType _TriggerType = null;
+		private KalturaIntegrationJobTriggerData _TriggerData;
+		#endregion
+
+		#region Properties
+		public string CallbackNotificationUrl
+		{
+			get { return _CallbackNotificationUrl; }
+			set 
+			{ 
+				_CallbackNotificationUrl = value;
+				OnPropertyChanged("CallbackNotificationUrl");
+			}
+		}
+		public KalturaIntegrationProviderType ProviderType
+		{
+			get { return _ProviderType; }
+			set 
+			{ 
+				_ProviderType = value;
+				OnPropertyChanged("ProviderType");
+			}
+		}
+		public KalturaIntegrationJobProviderData ProviderData
+		{
+			get { return _ProviderData; }
+			set 
+			{ 
+				_ProviderData = value;
+				OnPropertyChanged("ProviderData");
+			}
+		}
+		public KalturaIntegrationTriggerType TriggerType
+		{
+			get { return _TriggerType; }
+			set 
+			{ 
+				_TriggerType = value;
+				OnPropertyChanged("TriggerType");
+			}
+		}
+		public KalturaIntegrationJobTriggerData TriggerData
+		{
+			get { return _TriggerData; }
+			set 
+			{ 
+				_TriggerData = value;
+				OnPropertyChanged("TriggerData");
+			}
+		}
+		#endregion
+
+		#region CTor
+		public KalturaIntegrationJobData()
+		{
+		}
+
+		public KalturaIntegrationJobData(XmlElement node) : base(node)
+		{
+			foreach (XmlElement propertyNode in node.ChildNodes)
+			{
+				string txt = propertyNode.InnerText;
+				switch (propertyNode.Name)
+				{
+					case "callbackNotificationUrl":
+						this.CallbackNotificationUrl = txt;
+						continue;
+					case "providerType":
+						this.ProviderType = (KalturaIntegrationProviderType)KalturaStringEnum.Parse(typeof(KalturaIntegrationProviderType), txt);
+						continue;
+					case "providerData":
+						this.ProviderData = (KalturaIntegrationJobProviderData)KalturaObjectFactory.Create(propertyNode, "KalturaIntegrationJobProviderData");
+						continue;
+					case "triggerType":
+						this.TriggerType = (KalturaIntegrationTriggerType)KalturaStringEnum.Parse(typeof(KalturaIntegrationTriggerType), txt);
+						continue;
+					case "triggerData":
+						this.TriggerData = (KalturaIntegrationJobTriggerData)KalturaObjectFactory.Create(propertyNode, "KalturaIntegrationJobTriggerData");
+						continue;
+				}
+			}
+		}
+		#endregion
+
+		#region Methods
+		public override KalturaParams ToParams()
+		{
+			KalturaParams kparams = base.ToParams();
+			kparams.AddReplace("objectType", "KalturaIntegrationJobData");
+			kparams.AddStringIfNotNull("callbackNotificationUrl", this.CallbackNotificationUrl);
+			kparams.AddStringEnumIfNotNull("providerType", this.ProviderType);
+			if (this.ProviderData != null)
+				kparams.Add("providerData", this.ProviderData.ToParams());
+			kparams.AddStringEnumIfNotNull("triggerType", this.TriggerType);
+			if (this.TriggerData != null)
+				kparams.Add("triggerData", this.TriggerData.ToParams());
+			return kparams;
+		}
+		#endregion
+	}
+}
+
---
+++ KalturaClient/Types/KalturaVoicebaseJobProviderData.cs
@@ -0,0 +1,195 @@
+// ===================================================================================================
+//                           _  __     _ _
+//                          | |/ /__ _| | |_ _  _ _ _ __ _
+//                          | ' </ _` | |  _| || | '_/ _` |
+//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+//
+// This file is part of the Kaltura Collaborative Media Suite which allows users
+// to do with audio, video, and animation what Wiki platfroms allow them to do with
+// text.
+//
+// Copyright (C) 2006-2015  Kaltura Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @ignore
+// ===================================================================================================
+using System;
+using System.Xml;
+using System.Collections.Generic;
+
+namespace Kaltura
+{
+	public class KalturaVoicebaseJobProviderData : KalturaIntegrationJobProviderData
+	{
+		#region Private Fields
+		private string _EntryId = null;
+		private string _FlavorAssetId = null;
+		private string _TranscriptId = null;
+		private string _CaptionAssetFormats = null;
+		private string _ApiKey = null;
+		private string _ApiPassword = null;
+		private KalturaLanguage _SpokenLanguage = null;
+		private string _FileLocation = null;
+		private bool? _ReplaceMediaContent = false;
+		#endregion
+
+		#region Properties
+		public string EntryId
+		{
+			get { return _EntryId; }
+			set 
+			{ 
+				_EntryId = value;
+				OnPropertyChanged("EntryId");
+			}
+		}
+		public string FlavorAssetId
+		{
+			get { return _FlavorAssetId; }
+			set 
+			{ 
+				_FlavorAssetId = value;
+				OnPropertyChanged("FlavorAssetId");
+			}
+		}
+		public string TranscriptId
+		{
+			get { return _TranscriptId; }
+			set 
+			{ 
+				_TranscriptId = value;
+				OnPropertyChanged("TranscriptId");
+			}
+		}
+		public string CaptionAssetFormats
+		{
+			get { return _CaptionAssetFormats; }
+			set 
+			{ 
+				_CaptionAssetFormats = value;
+				OnPropertyChanged("CaptionAssetFormats");
+			}
+		}
+		public string ApiKey
+		{
+			get { return _ApiKey; }
+			set 
+			{ 
+				_ApiKey = value;
+				OnPropertyChanged("ApiKey");
+			}
+		}
+		public string ApiPassword
+		{
+			get { return _ApiPassword; }
+			set 
+			{ 
+				_ApiPassword = value;
+				OnPropertyChanged("ApiPassword");
+			}
+		}
+		public KalturaLanguage SpokenLanguage
+		{
+			get { return _SpokenLanguage; }
+			set 
+			{ 
+				_SpokenLanguage = value;
+				OnPropertyChanged("SpokenLanguage");
+			}
+		}
+		public string FileLocation
+		{
+			get { return _FileLocation; }
+			set 
+			{ 
+				_FileLocation = value;
+				OnPropertyChanged("FileLocation");
+			}
+		}
+		public bool? ReplaceMediaContent
+		{
+			get { return _ReplaceMediaContent; }
+			set 
+			{ 
+				_ReplaceMediaContent = value;
+				OnPropertyChanged("ReplaceMediaContent");
+			}
+		}
+		#endregion
+
+		#region CTor
+		public KalturaVoicebaseJobProviderData()
+		{
+		}
+
+		public KalturaVoicebaseJobProviderData(XmlElement node) : base(node)
+		{
+			foreach (XmlElement propertyNode in node.ChildNodes)
+			{
+				string txt = propertyNode.InnerText;
+				switch (propertyNode.Name)
+				{
+					case "entryId":
+						this.EntryId = txt;
+						continue;
+					case "flavorAssetId":
+						this.FlavorAssetId = txt;
+						continue;
+					case "transcriptId":
+						this.TranscriptId = txt;
+						continue;
+					case "captionAssetFormats":
+						this.CaptionAssetFormats = txt;
+						continue;
+					case "apiKey":
+						this.ApiKey = txt;
+						continue;
+					case "apiPassword":
+						this.ApiPassword = txt;
+						continue;
+					case "spokenLanguage":
+						this.SpokenLanguage = (KalturaLanguage)KalturaStringEnum.Parse(typeof(KalturaLanguage), txt);
+						continue;
+					case "fileLocation":
+						this.FileLocation = txt;
+						continue;
+					case "replaceMediaContent":
+						this.ReplaceMediaContent = ParseBool(txt);
+						continue;
+				}
+			}
+		}
+		#endregion
+
+		#region Methods
+		public override KalturaParams ToParams()
+		{
+			KalturaParams kparams = base.ToParams();
+			kparams.AddReplace("objectType", "KalturaVoicebaseJobProviderData");
+			kparams.AddStringIfNotNull("entryId", this.EntryId);
+			kparams.AddStringIfNotNull("flavorAssetId", this.FlavorAssetId);
+			kparams.AddStringIfNotNull("transcriptId", this.TranscriptId);
+			kparams.AddStringIfNotNull("captionAssetFormats", this.CaptionAssetFormats);
+			kparams.AddStringIfNotNull("apiKey", this.ApiKey);
+			kparams.AddStringIfNotNull("apiPassword", this.ApiPassword);
+			kparams.AddStringEnumIfNotNull("spokenLanguage", this.SpokenLanguage);
+			kparams.AddStringIfNotNull("fileLocation", this.FileLocation);
+			kparams.AddBoolIfNotNull("replaceMediaContent", this.ReplaceMediaContent);
+			return kparams;
+		}
+		#endregion
+	}
+}
+

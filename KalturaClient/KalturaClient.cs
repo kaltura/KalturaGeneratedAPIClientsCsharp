c3ec28e8733cfe10bbd8608fbbd3d24828a576f9
Kaltura CSI <community@kaltura.com>
2016-02-01 01:09:42 -0500

    New clientlib ver

--- KalturaClient/KalturaClient.cs
+++ KalturaClient/KalturaClient.cs
@@ -34,7 +34,7 @@ namespace Kaltura
 		public KalturaClient(KalturaConfiguration config) : base(config)
 		{
 				ApiVersion = "3.3.0";
-				ClientTag = "dotnet:16-01-31";
+				ClientTag = "dotnet:16-02-01";
 		}
 
 		KalturaAccessControlProfileService _AccessControlProfileService;

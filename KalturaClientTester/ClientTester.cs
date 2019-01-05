cd7dcafbbedb81f0ad2ea2b0b9170b29c8c00517
Kaltura CSI <community@kaltura.com>
2019-01-05 02:42:00 -0500

    New clientlib ver

--- KalturaClientTester/ClientTester.cs
+++ KalturaClientTester/ClientTester.cs
@@ -41,7 +41,7 @@ namespace Kaltura.Tester
     {
         private const int PARTNER_ID = @YOUR_PARTNER_ID@; //enter your partner id
         private const string ADMIN_SECRET = "@YOUR_ADMIN_SECRET@"; //enter your admin secret
-        private const string SERVICE_URL = "https://www.kaltura.com";
+        private const string SERVICE_URL = "@SERVICE_URL@";
         private const string USER_ID = "testUser";
 
         private static int code = 0;

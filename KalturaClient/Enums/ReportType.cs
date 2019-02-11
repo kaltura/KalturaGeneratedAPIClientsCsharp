34b97d50140f3c32a93e096c2f6b5ad1711c3822
Kaltura CSI <community@kaltura.com>
2019-02-11 01:43:56 -0500

    New clientlib ver

--- KalturaClient/Enums/ReportType.cs
+++ KalturaClient/Enums/ReportType.cs
@@ -70,6 +70,7 @@ namespace Kaltura.Enums
 		public static readonly ReportType TOP_CONTENT_CONTRIBUTORS = new ReportType("39");
 		public static readonly ReportType APP_DOMAIN_UNIQUE_ACTIVE_USERS = new ReportType("40");
 		public static readonly ReportType TOP_SOURCES = new ReportType("41");
+		public static readonly ReportType VPAAS_USAGE_MULTI = new ReportType("42");
 		public static readonly ReportType PARTNER_USAGE = new ReportType("201");
 
 		private ReportType(string name) : base(name) { }

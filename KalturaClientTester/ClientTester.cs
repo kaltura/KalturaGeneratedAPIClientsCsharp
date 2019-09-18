563124cfd76ee245c1221356c36b538f34cbda54
arthurvaverko <arthur.vaverko@kaltura.com>
2019-09-18 10:52:29 +0300

    updated tester to netcorapp to be able to run

--- KalturaClientTester/ClientTester.cs
+++ KalturaClientTester/ClientTester.cs
@@ -39,9 +39,9 @@ namespace Kaltura.Tester
 {
     class ClientTester : ILogger
     {
-        private const int PARTNER_ID = @YOUR_PARTNER_ID@; //enter your partner id
-        private const string ADMIN_SECRET = "@YOUR_ADMIN_SECRET@"; //enter your admin secret
-        private const string SERVICE_URL = "@SERVICE_URL@";
+         private const int PARTNER_ID = 2141161; //enter your partner id
+        private const string ADMIN_SECRET = "9342e4146b0a59a20696e27dd9773b03"; //enter your admin secret
+        private const string SERVICE_URL = "http://kaltura.com/";
         private const string USER_ID = "testUser";
 
         private static int code = 0;

655453c45cb8718669626a472ea97d938d9e9470
Kaltura CSI <community@kaltura.com>
2022-03-12 13:43:43 -0500

    New clientlib ver

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -35,7 +35,7 @@ namespace Kaltura
 	{
 		public Client(Configuration config) : base(config)
 		{
-				ApiVersion = "18.0.0";
+				ApiVersion = "18.1.0";
 				ClientTag = "dotnet:22-03-12";
 		}
 	
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Kaltura C# API Client Library.
-Compatible with Kaltura server version 18.0.0 and above.
+Compatible with Kaltura server version 18.1.0 and above.

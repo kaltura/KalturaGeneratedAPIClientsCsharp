ff23261b18e42a240be36604135f78dd3b051313
Kaltura CSI <community@kaltura.com>
2022-05-25 19:41:02 -0400

    New clientlib ver

--- KalturaClient/Client.cs
+++ KalturaClient/Client.cs
@@ -36,7 +36,7 @@ namespace Kaltura
 		public Client(Configuration config) : base(config)
 		{
 				ApiVersion = "18.4.0";
-				ClientTag = "dotnet:22-05-24";
+				ClientTag = "dotnet:22-05-25";
 		}
 	
 		#region Properties
